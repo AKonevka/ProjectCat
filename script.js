e16959b753de29b9fae07493ba1fb69cf0a7eff3
AKonevka <122662320+AKonevka@users.noreply.github.com>
2025-09-10 17:00:42 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 
 let block = document.getElementsByClassName('block');
-console.log(typeof block)
+let Filian = document.getElementById('Filian');
+console.log(Filian);
+console.log(typeof block);
 block.array;
 console.log(Array.from(block));
 block = Array.from(block);
 console.log(typeof block1);
 let spin = 3;
+let switchSpin = 0;
 
 block.forEach(element => {
     element.addEventListener('mouseover', function (e) {
@@ -15,13 +18,35 @@ block.forEach(element => {
     element.addEventListener('mouseout', function (e) {
         const img = element.querySelector('img');
         img.classList.remove('spin');
+        img.classList.remove('spinx');
         spin = 3;
         img.style.setProperty('--rotation-duration', `${spin}s`);
     })
     element.addEventListener('click', function (e) {
-        spin = Math.max(0.1, spin * 0.5);
-        const img = element.querySelector('img');
-        img.style.setProperty('--rotation-duration', `${spin}s`);
+        if (element == Filian) {
+            if (switchSpin) {
+                spin = Math.max(0.1, spin * 0.5);
+                const img = element.querySelector('img');
+                img.style.setProperty('--rotation-duration', `${spin}s`);
+                img.classList.remove('spinx');
+                img.classList.add('spin');
+
+                switchSpin = switchSpin - 1;
+            } else {
+                spin = Math.max(0.1, spin * 0.5);
+                const img = element.querySelector('img');
+                img.style.setProperty('--rotation-duration', `${spin}s`);
+                img.classList.remove('spin');
+                img.classList.add('spinx');
+
+                switchSpin = switchSpin + 1;
+            }
+        } else {
+            spin = Math.max(0.1, spin * 0.5);
+            const img = element.querySelector('img');
+            img.style.setProperty('--rotation-duration', `${spin}s`);
+        }
+
     });
 
 })
